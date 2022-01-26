85a16422c2f2946d164cf2cfa03f8304f159c3d7
Laëtitia PHIMMASANE <laetitia.phimmasane@epsi.fr>
2022-01-27 00:28:41 +0100

    init

---
+++ src/com/epsi/gosecuri/web/htmlFile.java
@@ -0,0 +1,18 @@
+package com.epsi.gosecuri.web;
+
+public class htmlFile {
+    //Attributs
+    String htmlfile="",cssStyle="",inputCheckbox,header, body, footer;
+
+    //Constructeur
+    public htmlFile(){}
+
+    //Méthodes
+    private void addButton(String content, String css){ }
+    private void addImage(){}
+    private void addPersonCheckbox(String name){}
+
+}
+
+
+
