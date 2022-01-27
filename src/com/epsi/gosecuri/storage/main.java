a3cd5ef6ab6b02932762eeeb4a71af6948a9efff
Laëtitia PHIMMASANE <laetitia.phimmasane@epsi.fr>
2022-01-27 11:56:16 +0100

    partie storage presque fonctionnelle & pseudo-code web&security

--- src/com/epsi/gosecuri/storage/main.java
+++ src/com/epsi/gosecuri/storage/main.java
@@ -2,46 +2,76 @@ package com.epsi.gosecuri.storage;
 
 import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class main {
-    public static void main (String[] args){
-        //TODO : Récupérer les fichiers distants du Git
-        //TODO : Stocker dans un format JSON
-        //TODO : Réinjecter dans le HTML
+    public static void main (String[] args) throws Exception{
 
-        String html = "<div><h1>C'est un fichier HTML</h1><p>Petit paragraphe</p></div>";
-        File file = new File("C:\\Users\\Lele\\IdeaProjects\\GoSecuri\\com.epsi.javahtmlfiles\\test.html");
+        //Paramètres
+        HashMap<String, ArrayList<String>> agents = new HashMap<String,ArrayList<String>>();
 
-        try
-        {
-            URL materiel = new URL("https://raw.githubusercontent.com/Adigbonon/GoSecuri/files/liste.txt");
-            URL agents = new URL("https://raw.githubusercontent.com/Adigbonon/GoSecuri/files/liste_agents.txt");
+        //Récupère le fichier list.txt et staff.txt
+        URL list = getFileFromGit("liste");
+        URL staff = getFileFromGit("staff");
 
-            //Cloner le fichier ou lire ligne par ligne ??? GitHubAPI
-            readFileInConsole(materiel);
-            readFileInConsole(agents);
+        //Lire chaque ligne de staff.txt (lecture fichier)
+        agents = readStaffFromGit(staff); // stocker dans une map chaque ligne
 
-            //Stocker les données dans un format JSON
+        //boucle cherche le fichier qui porte le même nom que l'élément
+        try {
+            for(Map.Entry<String, ArrayList<String>> entry : agents.entrySet()) {
+                String key = entry.getKey();
+                int beginLengthFile = getFileFromGit(key).getFile().length() - (key.length() +4);
+                int endLengthFile = getFileFromGit(key).getFile().length() -4;
+                String fileNameGit =getFileFromGit(key).getFile().substring(beginLengthFile, endLengthFile) ;
 
-
-            //Injecter dans le HTML
-            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-            bw.write(html);
-            bw.close();
-
+                if (key == fileNameGit) {
+                    URL url = getFileFromGit(key);
+                    ArrayList<String> infoAgent = readInfoAgentFromGit(url); //lire chaque ligne et l'ajouter à une liste
+                    agents.replace(key, infoAgent); // stocke la liste dans la map à l'emplacement correspondant
+                }
+            }
+        } catch(Exception NullPointerException){
+            throw new NullPointerException("Il manque un fichier correspondant au membre du staff " + agents.keySet());
         }
-        catch (Exception e){
-            e.printStackTrace();
+
+        System.out.println(agents);
+        //réécrire tout ça dans un fichier Recap stocké jsp où pour le prochain trigger
+    }
+
+    private static URL getFileFromGit(String filename) throws Exception{
+        try{
+            URL address = new URL("https://raw.githubusercontent.com/Adigbonon/GoSecuri/files/"+filename+".txt");
+            return address;
+        } catch(Exception MalformedURLExpression){
+            throw new Exception("L'URL est incorrecte.");
         }
     }
 
-    public static void readFileInConsole(URL url) throws java.io.IOException {
-        String line = "";
+    private static HashMap<String, ArrayList<String>> readStaffFromGit(URL url) throws java.io.IOException{
+        String line="";
+        HashMap<String, ArrayList<String>> staffList = new HashMap<String, ArrayList<String>>();
+        ArrayList<String> listInformations = new ArrayList<String>();
+
         InputStream isStream = url.openStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(isStream));
-        while ((line = br.readLine()) !=null)
-        {
-            System.out.println(line);
+        BufferedReader br = new BufferedReader((new InputStreamReader(isStream)));
+        while((line = br.readLine()) != null){
+            staffList.put(line, listInformations);
         }
+        return staffList;
+    }
+
+    private static ArrayList<String> readInfoAgentFromGit(URL url) throws java.io.IOException{
+        String line="";
+        ArrayList<String> agentInfosList = new ArrayList<String>();
+
+        InputStream isStream = url.openStream();
+        BufferedReader br = new BufferedReader((new InputStreamReader(isStream)));
+        while((line = br.readLine()) != null){
+            agentInfosList.add(line);
+        }
+        return agentInfosList;
     }
 }
--- src/com/epsi/gosecuri/web/main.java
+++ src/com/epsi/gosecuri/web/main.java
@@ -6,36 +6,14 @@ import java.net.URL;
 
 public class main {
     public static void main (String[] args){
-        //TODO : Récupérer les fichiers distants du Git
-        //TODO : Stocker dans un format JSON
-        //TODO : Réinjecter dans le HTML
+        int nbStaff = 0;
+        //Récup le fichier Recap depuis le git
+        //Sortir les listes avec les clés-nom
 
+        //Boucle pour chaque liste <= nbStaff
+            //créer un html avec la classe htmlFile
+            //stocker le html dans le git (wsh comment ??) ==> un dossier bien rangé et tout
 
-
-        String html = "<div><h1>C'est un fichier HTML</h1><p>Petit paragraphe</p></div>";
-        File file = new File("C:\\Users\\Lele\\IdeaProjects\\GoSecuri\\com.epsi.javahtmlfiles\\test.html");
-
-        try
-        {
-            URL materiel = new URL("https://raw.githubusercontent.com/Adigbonon/GoSecuri/files/liste.txt");
-            URL agents = new URL("https://raw.githubusercontent.com/Adigbonon/GoSecuri/files/liste_agents.txt");
-
-            //Cloner le fichier ou lire ligne par ligne ??? GitHubAPI
-            readFileInConsole(materiel);
-            readFileInConsole(agents);
-
-            //Stocker les données dans un format JSON
-
-
-            //Injecter dans le HTML
-            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-            bw.write(html);
-            bw.close();
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
     }
 
     public static void readFileInConsole(URL url) throws java.io.IOException {
